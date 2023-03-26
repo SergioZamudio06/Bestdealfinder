018ab3201917237cf2765de0f577173dede2356d
Sergio <sergio_zamudio@hotmail.com>
2023-03-25 23:13:05 -0500

    4.0 version

---
+++ src/app/servicios/usuario.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  url='http://localhost/bestdealfinder/src/app/php/usuario/'
+
+  constructor(private http:HttpClient) { }
+
+  consultar () {
+    return this.http.get(`${this.url}Consulta.php`);
+  }
+  insertar(articulo:any){
+    return this.http.post(`${this.url}insertar.php`, JSON.stringify(articulo));
+  }
+  eliminar(id:number) {
+    return this.http.get(`${this.url}eliminar.php?id=${id}`);
+  }
+  edit(datos:any, id:number){
+    return this.http.post(`${this.url}editar.php?id=${id}`, JSON.stringify(datos));
+  }
+
+}
